624c831e8363bf7a0dad7d65532d006a1bc4f6d6
vaxerski <43317083+vaxerski@users.noreply.github.com>
2026-03-27 19:01:42 +0000

    [gha] Update inputs

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
     };
 
     hyprland = {
-      url = "github:hyprwm/hyprland/v0.54.2";
+      url = "github:hyprwm/hyprland/v0.54.3";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.systems.follows = "systems";
       inputs.aquamarine.follows = "aquamarine";
