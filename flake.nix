03ff28ba339ef07f0a3c06744833447d489ebcec
vaxerski <43317083+vaxerski@users.noreply.github.com>
2026-08-13 18:00:57 +0000

    [gha] Update inputs

--- flake.nix
+++ flake.nix
@@ -174,7 +174,7 @@
     };
 
     hyprutils = {
-      url = "github:hyprwm/hyprutils/v0.14.0";
+      url = "github:hyprwm/hyprutils/v0.14.1";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.systems.follows = "systems";
     };
